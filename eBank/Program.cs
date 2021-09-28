a818d69ccf92341109801dc55367495d2deae5fe
hoprob <75954299+hoprob@users.noreply.github.com>
2021-09-28 19:34:08 +0200

    Add project files.

---
+++ eBank/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace eBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            DefUsers(users);
+            Console.WriteLine($"\t{Welcome()}");
+            LogIn(users);
+        }
+        //Method for log in
+        private static bool LogIn(List<User> users)
+        {
+            bool loggedIn = false;
+            bool correctId = false;
+            //userNum to get the number of correct user-object in list.
+            int userNum = 0;
+            do
+            {
+                Console.Write("\nSkriv in personnummer (12 siffror ÅÅÅÅMMDDXXXX): ");
+                string id = Console.ReadLine();
+                //Checks if the id is in the right format
+                if (!User.ValidateId(id))
+                {
+                    Console.WriteLine("\n\tERROR! Personnummret är inte i korrekt format!");
+                    correctId = false;
+                }
+                else
+                {
+                    //Searching for id in list of users
+                    for (int i = 0; i < users.Count; i++)
+                    {
+                        if(users[i].Id == id)
+                        {
+                            correctId = true;
+                            userNum = i;
+                        }
+                    }
+                }
+            } while (!correctId);
+            Console.WriteLine("\n\tGiltligt personummer!");
+            /*Get pincode input, checks format of input and compare to user-object
+             * user gets 3 attempts*/
+            bool correctPin = false;
+            int attempts = 0;
+            string input;
+            int pin;
+            do
+            {
+                Console.Write("\nSkriv in pinkod (4 siffror): ");
+                input = Console.ReadLine();
+                if(!Int32.TryParse(input, out pin) || input.Length != 4)
+                {
+                    Console.WriteLine("\n\tERROR! Du måste skriva 4st siffor!");
+                    correctPin = false;
+                }
+                else
+                {
+                    if(users[userNum].Pin == pin)
+                    {
+                        Console.WriteLine("\n\tPinkod giltlig!");
+                        correctPin = true;
+                        Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\n\tFel pinkod! Du har {2 - attempts} försök kvar.");
+                        correctPin = false;
+                        attempts++;
+                    }
+                }
+            } while (!correctPin && attempts < 3);
+            //Locks program for 3 min. With printed countdown.
+            if (!correctPin)
+            {               
+                int counterMin = 2;
+                int counterSec = 59;
+                for (int i = 0; i <= 180; i++)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Du har angett fel pinkod 3 gånger. Programmet är nu låst i 3 minuter.");
+                    Console.WriteLine($"{counterMin}m {counterSec}s");
+                    if(counterSec == 0 && counterMin > 0)
+                    {
+                        counterSec = 59;
+                        counterMin--;
+                    }
+                    else
+                    {
+                        counterSec--;
+                    }
+                    Thread.Sleep(1000);
+                }
+            }
+            //Checks that both id and pin is correct.
+            if(correctId && correctPin)
+            {
+                loggedIn = true;
+            }
+            else
+            {
+                loggedIn = false;
+            }
+            return loggedIn;
+        }
+        //Method for a random welcome-message
+        private static string Welcome()
+        {
+            Random rnd = new Random();
+            string[] message = { "Välkommen!", "God dag och Välkommen!", "Kul att se dig, Välkommen!", "Redo för lite finanser? Välkommen!", "Hej och välkommen!" };
+            string welcomeMsg = message[rnd.Next(0, message.Length - 1)];
+            return welcomeMsg;
+        }
+        //Method for adding default users in users-list
+        private static void DefUsers(List<User> users)
+        {
+            User user1 = new User("Robin", "Svensson", "198112189876", 6532);
+            User user2 = new User("Kalle", "Karlsson", "198902132458", 5617);
+            User user3 = new User("Petra", "Andersson", "199202296928", 1867);
+            User user4 = new User("Hilda", "Abrahamsson", "196212214966", 7612);
+            User user5 = new User("Frans", "Fransson", "200001010115", 1234);
+            users.Add(user1);
+            users.Add(user2);
+            users.Add(user3);
+            users.Add(user4);
+            users.Add(user5);
+        }
+    }
+}
---
+++ eBank/User.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eBank
+{
+    class User
+    {
+        string firstName;
+        string lastName;
+        string id;
+        int pin;
+
+        public User(string firstName, string lastName, string id, int pin)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = id;
+            this.pin = pin;
+        }
+        public User()
+        {
+
+        }
+        public string Id
+        {
+            get { return this.id; }
+        }
+        public int Pin
+        {
+            get { return this.pin; }
+        }
+        public static bool ValidateId(string id)
+        {
+            bool correctId = false;
+            try
+            {
+                if (id.Length == 12)
+                {
+                    //Seperates parts of id number
+                    int year = Int32.Parse(id.Substring(0, 4));
+                    int month = Int32.Parse(id.Substring(4, 2));
+                    int day = Int32.Parse(id.Substring(6, 2));
+                    //Controlnumber is the last digit in id number
+                    int controlNum = Int32.Parse(id.Substring(11, 1));
+                    //Changes the string to an array with the seperat digits
+                    string controlString = id.Substring(2, 9);
+                    int[] controlArray = new int[9];
+                    for (int i = 0; i < 9; i++)
+                    {
+                        controlArray[i] = Int32.Parse(controlString.Substring(i, 1));
+                    }
+                    //The process to get the control sum to check Controlnumber
+                    int controlSum= 0;
+                    int addNum;
+                    for (int i = 0; i < 9; i++)
+                    {
+                        //First digit will multiply by 2, second 1, third 2 and so on.
+                        if (i % 2 == 0)
+                        {
+                            addNum = controlArray[i] * 2;
+                        }
+                        else
+                        {
+                            addNum = controlArray[i] * 1;
+                        }
+                        //If the product is two digits, you must add them up before add to controlSum
+                        if (addNum > 9)
+                        {
+                            string numString = addNum.ToString();
+                            int num1 = Int32.Parse(numString.Substring(0, 1));
+                            int num2 = Int32.Parse(numString.Substring(1, 1));
+                            controlSum += num1 + num2;
+                        }
+                        //If the product is 1 digit, it ads up to the controlSum
+                        else
+                        {
+                            controlSum += addNum;
+                        }
+                    }
+                    //Rounds to upper 10.
+                    int upperControlNum = (controlSum / 10 + 1) * 10;
+                    //The control number must be the same as the controlsum rounded to upper 10 minus the controlsum.
+                    if(controlNum == upperControlNum - controlSum)
+                    {
+                        //Checks that month and day is correct.
+                        if (month >= 1 && month <= 12 && day > 0)
+                        {
+                            if (month % 2 != 0 || month == 8 && day <= 31)
+                            {
+                                correctId = true;
+                            }            
+                            else if (month % 2 == 0 && day <= 30)
+                            {
+                                correctId = true;
+                                bool leapYear = LeapYear(year);
+                                if (!leapYear && month == 02 && day > 28)
+                                {
+                                    correctId = false;
+                                }
+                                else if (leapYear && month == 2 && day > 29)
+                                {
+                                    correctId = false;
+                                }   
+                            }
+
+                        }
+                    }
+                                        
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\n\tERROR! Du måste skriva personnummer med siffor!");
+            }
+            return correctId;
+        }
+        public static bool LeapYear(int year)
+        {
+            bool isLeepYear = false;
+            try
+            {
+                double yearDbl = Convert.ToDouble(year);
+                if (yearDbl % 4 == 0)
+                {
+                    isLeepYear = true;
+                    if (yearDbl % 100 == 0)
+                    {
+                        isLeepYear = false;
+                        if (yearDbl % 400 == 0)
+                        {
+                            isLeepYear = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return isLeepYear;
+        }
+    }
+}
